4af720665458afaaf20fc4bd31cb587455d7b656
kimoVoid <bordakmc@gmail.com>
2024-08-21 09:29:57 -0300

    Trade button cleanup

--- src/main/java/me/kimovoid/microhud/villagergui/GuiTradeButton.java
+++ src/main/java/me/kimovoid/microhud/villagergui/GuiTradeButton.java
@@ -11,26 +11,4 @@ public class GuiTradeButton extends GuiButton {
         this.ordinal = ordinal;
         this.visible = true;
     }
-
-    /*
-    @Override
-    public void renderToolTip(int mouseX, int mouseY) {
-        if (this.isHovered && MerchantScreen.this.container.getRecipes().size() > this.field_19165 + MerchantScreen.this.field_19163) {
-            if (mouseX < this.x + 20) {
-                ItemStack itemStack = ((TradeOffer)MerchantScreen.this.container.getRecipes().get(this.field_19165 + MerchantScreen.this.field_19163))
-                        .getAdjustedFirstBuyItem();
-                MerchantScreen.this.renderTooltip(itemStack, mouseX, mouseY);
-            } else if (mouseX < this.x + 50 && mouseX > this.x + 30) {
-                ItemStack itemStack = ((TradeOffer)MerchantScreen.this.container.getRecipes().get(this.field_19165 + MerchantScreen.this.field_19163)).getSecondBuyItem();
-                if (!itemStack.isEmpty()) {
-                    MerchantScreen.this.renderTooltip(itemStack, mouseX, mouseY);
-                }
-            } else if (mouseX > this.x + 65) {
-                ItemStack itemStack = ((TradeOffer)MerchantScreen.this.container.getRecipes().get(this.field_19165 + MerchantScreen.this.field_19163))
-                        .getMutableSellItem();
-                MerchantScreen.this.renderTooltip(itemStack, mouseX, mouseY);
-            }
-        }
-    }
-     */
 }
